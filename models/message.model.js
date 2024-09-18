e636c80c7d684d5e231b432e143a48b3fe3047b9
karann09 <karankumar011kp@gmail.com>
2024-09-18 14:12:50 +0530

    added timestamp in model

--- models/message.model.js
+++ models/message.model.js
@@ -27,6 +27,6 @@ const messageSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-})
+}, {timestamps: true})
 
 export const Message = mongoose.model("Message", messageSchema)
