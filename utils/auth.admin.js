c305273e9a2b9293fdb5031d97699762eee27f25
karann09 <karankumar011kp@gmail.com>
2024-09-21 17:25:33 +0530

    auth for patient

--- utils/auth.admin.js
+++ utils/auth.admin.js
@@ -19,3 +19,20 @@ export const isAdmin = asyncHandler(async(req,res, next)=>{
     next()
 
 });
+
+export const isPatient = asyncHandler(async(req,res, next)=>{
+    const token = req.cookie.patientToken;
+    if(!token){
+        throw new ApiError(400, "User is not authenticated")
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded._id)
+
+    if(req.user.role !== "Patient"){
+        throw new ApiError(403, "User restriction")
+    }
+
+    next()
+
+});
