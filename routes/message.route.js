659e48bccd134e2628c58731b0d397febb8c65fa
karann09 <karankumar011kp@gmail.com>
2024-09-22 17:24:49 +0530

    added secure route

--- routes/message.route.js
+++ routes/message.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getMessage, sendMessage } from "../controller/message.controller.js";
+import { isAdmin } from "../utils/auth.admin.js";
 
 const router = express.Router();
 
 router.post("/send", sendMessage);
-router.get("/getall", getMessage)
+router.get("/getall", isAdmin, getMessage)
 
 export default router;
