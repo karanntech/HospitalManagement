06833c0ddfdbeef27167132a8fc07715a757bb02
karann09 <karankumar011kp@gmail.com>
2024-09-23 21:26:39 +0530

    cloudinary setup

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import app from "./app.js";
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    secret_key: process.env.CLOUDINARY_API_SECRET
-})
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    })
 
 
 app.listen(process.env.PORT, ()=> {
