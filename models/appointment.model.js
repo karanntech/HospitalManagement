72de7b6451e8141c14190813da71bc8aad28bdf5
karann09 <karankumar011kp@gmail.com>
2024-09-24 20:22:34 +0530

    model created for appointment

---
+++ models/appointment.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import { User } from "./user.model";
+
+const appointmentSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    birthdate: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["Male", "Female"]
+    },
+    appointmentDate: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    docName: {
+        type: String,
+        required: true
+    },
+    hasVisited: {
+        type: Boolean,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    doctorId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
+    },
+    patientId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "Accepted", "Rejected"],
+        default: "Pending",
+      },
+});
+
+
+export const Appointment = mongoose.model("Appointment", appointmentSchema)
